8b9351bfd8da321ef5b22591b323399271cccc82
David O McLaurin <david.mclaurin@gmail.com>
2013-12-06 18:42:37 -0600

    added new build script, added a few references, reused reference file, removed antiquted files

--- makescript.sh
+++ makescript.sh
@@ -1,12 +1,42 @@
 if [ $# -eq 1 ]
 then
-  DOCUMENT=${1}
+  DOCUMENT_NAME=$1
 else
-  DOCUMENT="Document.tex"
+  DOCUMENT_NAME=Document
 fi
-
-pdflatex $DOCUMENT
-if [ $? -eq 0 ]
-then
-  open ${DOCUMENT%%.tex}.pdf
+echo "pdflatex $DOCUMENT_NAME"
+pdflatex $DOCUMENT_NAME
+if [ $? -eq 0 ]; then
+  citation=$(cat $DOCUMENT_NAME.aux | grep '\citation')
+  bibdata=$(cat $DOCUMENT_NAME.aux | grep '\bibdata')
+  bibstyle=$(cat $DOCUMENT_NAME.aux | grep '\bibstyle')
+  bibtexneeded=0
+  if [ -n "$citation" ]; then
+    echo "Citation: "$citation
+    bibtexneeded=1
+  fi
+  if [ -n "$bibdata" ]; then
+    echo "Bibdata: "$bibdata
+    bibtexneeded=2
+  fi
+  if [ $bibtexneeded -gt 0  ]; then
+    echo $bibtexneeded
+    echo "bibtex $DOCUMENT_NAME"
+    bibtex $DOCUMENT_NAME
+    if [ $? -eq 0 ]; then
+      echo "pdflatex $DOCUMENT_NAME"
+      pdflatex $DOCUMENT_NAME
+      if [ $? -eq 0 ]; then
+        echo "pdflatex $DOCUMENT_NAME"
+        pdflatex $DOCUMENT_NAME
+      else
+        exit $?
+      fi
+    else
+      exit $?
+    fi
+  fi
+else
+  exit $?
 fi
+open $DOCUMENT_NAME.pdf
